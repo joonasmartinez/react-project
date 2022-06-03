70fea52b0de1a4a303112f699635069f83c1ba24
Jonas <mz-martinez@live.com>
2022-06-03 10:03:07 -0300

    Refatored

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import { useState } from "react";
 
 function App() {
-  const [cep, setCep] = useState("");
+  const [cepI, setCep] = useState("");
   const [data, setData] = useState([]);
   const [erro, setErro] = useState("");
 
-   const updateDatas = async (cep)=>{
-    try{
-      setErro("")
-    const {logradouro, bairro, localidade} = await fetch(`https://viacep.com.br/ws/${cep.trim().replace("-","")}/json`, {method:'GET'}).then((response) => response.json());
-  
+   const updateDatas = async (cepIn)=>{
+     console.log("CEP TENTATIVA:",cepIn)
+    setErro("")
+      
+    const { cep, logradouro, bairro, localidade} = await fetch(`https://viacep.com.br/ws/${cepIn.trim().replaceAll("-","").replaceAll(",","").replace(".","")}/json`, {method:'GET'}).then((response) => response.json());
+    console.log(cep, logradouro, bairro, localidade)
     
-      if([logradouro, bairro, localidade].every((a)=>{return (a != undefined)}) ){
+      if([cep, logradouro, bairro, localidade].every((a)=>{return (a != undefined)}) ){
 
-        setData({logradouro, bairro, localidade})
+        setData({cep, logradouro, bairro, localidade})
+        return document.querySelector(".Info").removeAttribute("hidden")
         
       }
 
-    }catch(e){
-      setErro(`Algo inesperado aconteceu.`)
-    }
-    
+      document.querySelector(".Info").setAttribute("hidden", 0)
+    return setErro(`Algo inesperado aconteceu.`)
   }
 
   return (
@@ -28,13 +28,13 @@ function App() {
 
       <header>
         <h1>BUSCAR CEP</h1>
-        <input placeholder="Insira o CEP" onChange={(e)=>{setCep(e.target.value)}}></input><button onClick={()=>{updateDatas(cep.trim().replaceAll("-","").replaceAll(",","").replace(".",""))}}>OK</button>
+        <input placeholder="Insira o CEP" onChange={(e)=>{setCep(e.target.value)}}></input><button onClick={()=>{updateDatas(cepI)}}>OK</button>
         <br></br>
         <h6>{erro}</h6>
       </header>
 
-      <div className="Info">
-        <h2>{cep}</h2>
+      <div className="Info" hidden>
+        <h2>{data.cep}</h2>
         <span>{`${data.logradouro} - `}</span>
         <span>{`${data.bairro} / `}</span>
         <span>{`${data.localidade}. `}</span>
