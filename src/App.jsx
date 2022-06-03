41681aa20c67263c82cda043627bf7da1d534d7d
Jonas <mz-martinez@live.com>
2022-06-03 00:04:29 -0300

    Refatored

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,46 @@
 import { useState } from "react";
 
-async function search(cep){
-
-  if(cep.length >= 8) {
-    document.getElementsByTagName('h6')[0].setAttribute("hidden",0);
-      let a = await fetch(`https://viacep.com.br/ws/${cep.trim().replace("-","")}/json`, {method:'GET'}).then((response) => response.json());;
-      console.log(a);
-      if(!(a.erro)){
-        document.getElementsByClassName('Info')[0].getElementsByTagName("span")[0].innerHTML = `${a.logradouro} - `;
-        document.getElementsByClassName('Info')[0].getElementsByTagName("span")[1].innerHTML = `Bairro ${a.bairro}/`;;
-        document.getElementsByClassName('Info')[0].getElementsByTagName("span")[2].innerHTML = `Cidade ${a.localidade}`;;
-        document.getElementsByClassName('Info')[0].removeAttribute("hidden")
-      }
-      else{
-        document.getElementsByClassName('Info')[0].setAttribute("hidden",0)
-        document.getElementsByTagName('h6')[0].removeAttribute("hidden");
-      }
-    }
-}
-
 function App() {
   const [cep, setCep] = useState("");
+  const [data, setData] = useState([]);
+  const [erro, setErro] = useState("");
+
+   const updateDatas = async (cep)=>{
+    try{
+      setErro("")
+    const {logradouro, bairro, localidade} = await fetch(`https://viacep.com.br/ws/${cep.trim().replace("-","")}/json`, {method:'GET'}).then((response) => response.json());
+  
+    
+      if([logradouro, bairro, localidade].every((a)=>{return (a != undefined)}) ){
+
+        setData({logradouro, bairro, localidade})
+        
+      }
+
+    }catch(e){
+      setErro(`Algo inesperado aconteceu.`)
+    }
+    
+  }
 
   return (
     <div className="App">
 
       <header>
         <h1>BUSCAR CEP</h1>
-        <input placeholder="Insira o CEP" onChange={(e)=>{setCep(e.target.value)}}></input><button onClick={()=>{search(cep.trim().replace("-","").replace(",","").replace(".",""))}}>OK</button>
+        <input placeholder="Insira o CEP" onChange={(e)=>{setCep(e.target.value)}}></input><button onClick={()=>{updateDatas(cep.trim().replaceAll("-","").replaceAll(",","").replace(".",""))}}>OK</button>
         <br></br>
-        <h6 hidden>Digite um CEP válido.</h6>
+        <h6>{erro}</h6>
       </header>
 
-      <div className="Info" hidden>
+      <div className="Info">
         <h2>{cep}</h2>
-        <span>Rua: Santa Cruz - </span>
-        <span>Bairro: Santa Cruz, </span>
-        <span>Cidade: Gravataí</span>
+        <span>{`${data.logradouro} - `}</span>
+        <span>{`${data.bairro} / `}</span>
+        <span>{`${data.localidade}. `}</span>
       </div>
 
-    <h5>Developer Jonas Martinez</h5>
+    <footer>Desenvolvido por Jonas Martinez</footer>
     </div>
   );
 }
